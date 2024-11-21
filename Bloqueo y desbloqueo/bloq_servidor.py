56bcc8c3ffbf813952e546db034002210c03367c
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-21 13:00:03 -0600

    Update bloq_servidor.py

--- Bloqueo y desbloqueo/bloq_servidor.py
+++ Bloqueo y desbloqueo/bloq_servidor.py
@@ -21,3 +21,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         conn.sendall(comando.encode())
         respuesta = conn.recv(1024).decode()
         print(f"Respuesta del cliente: {respuesta}")
+    except KeyboardInterrupt:
