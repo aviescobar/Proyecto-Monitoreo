2c35580478fc5669ef1416e062bbec81ca40d64d
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-20 22:21:11 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -5,3 +5,4 @@ from tkinter import messagebox
 def ejecutar_comando_ssh(host, usuario, contraseña, comando):
   try:
     ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
