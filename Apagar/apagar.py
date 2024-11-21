990fb49561d0a1230bb87b924c9cb92d51250235
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-21 12:13:05 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -74,6 +74,8 @@ contraseña_entry.bind("<KeyRelease>", habilitar_boton)
 btn_apagar = tk.Button(root, text="Apagar", command=ejecutar_apagado, state=tk.DISABLED)
 btn_apagar.pack(pady=20)
 
+root.mainloop()
+
 
 
 
