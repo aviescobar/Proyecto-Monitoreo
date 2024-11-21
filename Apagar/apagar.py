7ba8a46230a30a97767008e7bad4b50068090830
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-20 22:22:43 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -6,3 +6,4 @@ def ejecutar_comando_ssh(host, usuario, contraseña, comando):
   try:
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname=host, username=usuario, password=contraseña)
