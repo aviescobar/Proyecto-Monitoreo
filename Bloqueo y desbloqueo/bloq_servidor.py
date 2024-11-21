282d4375f98d8bdb44d413db182f2588eac402f4
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-11-21 12:21:19 -0600

    Update bloq_servidor.py

--- Bloqueo y desbloqueo/bloq_servidor.py
+++ Bloqueo y desbloqueo/bloq_servidor.py
@@ -9,3 +9,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
   server.bind((HOST, PORT))
   server.listen(1)
   print(f"Servidor en ejecución en {HOST}:{PORT}")
+  conn, addr = server.accept()
