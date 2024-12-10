52107aa1c694f3d8163d480b8498560149dad0f6
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-09 22:48:41 -0600

    Update bloq_servidor.py

--- Bloqueo y desbloqueo/bloq_servidor.py
+++ Bloqueo y desbloqueo/bloq_servidor.py
@@ -23,4 +23,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         print(f"Respuesta del cliente: {respuesta}")
     except KeyboardInterrupt:
         print("\nServidor detenido.")
-      La documentación utiliza un lenguaje inadecuado, poco cla
+      La documentación utiliza un lenguaje inadecuado, poco c
