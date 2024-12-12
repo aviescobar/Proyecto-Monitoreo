b70210a23ce2b339c4c1b31d23c0dd6aa29d9b8c
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-11 21:51:14 -0600

    Update bloq_servidor.py

--- Bloqueo y desbloqueo/bloq_servidor.py
+++ Bloqueo y desbloqueo/bloq_servidor.py
@@ -23,4 +23,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
         print(f"Respuesta del cliente: {respuesta}")
     except KeyboardInterrupt:
         print("\nServidor detenido.")
-      La documentació
+      La documentac
