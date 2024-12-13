357fdf302e760c0567ccc9b08bb71b2213db2c87
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-12 21:42:36 -0600

    Update apagar.py

--- Apagar/apagar.py
+++ Apagar/apagar.py
@@ -60,7 +60,7 @@ class ApagarPCApp:
          # Conexión SSH
          ssh_client = paramiko.SSHClient()
          ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
+         ssh_client.connect(ip, username=user, password=password)
 
 
 
