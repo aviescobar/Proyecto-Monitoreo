afb31b4c0eb375454e7a9853d5d190a134dcbbf1
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-13 13:19:07 -0600

    Update receptor.py

--- Exhibir/receptor.py
+++ Exhibir/receptor.py
@@ -6,3 +6,4 @@ import cv2
 def run_receiver():
   receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   receiver_socket.connect(('172.168.2.242', 5001))  # Reemplaza 'IP_DEL_SERVIDOR' con la IP del servidor
+  print("Conectado al servidor para recibir la pantalla.")
