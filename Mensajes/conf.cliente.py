39522164f995ba8c03b2e4b9f650bc02521186e0
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-14 21:14:29 -0600

    Update conf.cliente.py

--- Mensajes/conf.cliente.py
+++ Mensajes/conf.cliente.py
@@ -6,3 +6,4 @@ HOST = '172.168.2.152'  # IP del servidor
 PORT = 12345      # Puerto del servidor
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((HOST, PORT))
