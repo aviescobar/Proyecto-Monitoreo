5922508a38dc639c4a31b5bde0125ff52ab824d9
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-15 21:48:39 -0600

    Update conf.cliente.py

--- Mensajes/conf.cliente.py
+++ Mensajes/conf.cliente.py
@@ -28,4 +28,4 @@ def enviar_mensajes():
           client_socket.close()
           break
 
-# Iniciar hilos para enviar y recibir mensajes simulta
+# Iniciar hilos para enviar y recibir mensajes simultanea
