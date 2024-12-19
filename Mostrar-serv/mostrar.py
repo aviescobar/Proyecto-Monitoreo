66b0c5f60851fbb580d6982cf5817111a8b9fbad
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-18 22:07:50 -0600

    Update mostrar.py

--- Mostrar-serv/mostrar.py
+++ Mostrar-serv/mostrar.py
@@ -9,3 +9,4 @@ def start_server(host='0.0.0.0', port=9999):
   server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
   server_socket.bind((host, port))
   server_socket.listen(1)
+  ###hoy
