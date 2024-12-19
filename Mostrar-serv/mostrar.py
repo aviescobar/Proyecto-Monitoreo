dd33c47332011040f72acc840c28e384e10eccab
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-18 21:55:36 -0600

    Update mostrar.py

--- Mostrar-serv/mostrar.py
+++ Mostrar-serv/mostrar.py
@@ -7,4 +7,4 @@ import numpy as np  # Importa numpy para la conversion de la imagen
 def start_server(host='0.0.0.0', port=9999):
   # Crear socket del servidor
   server_socket = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-  server_socket.bind
+  server_socket.bind((host, port))
