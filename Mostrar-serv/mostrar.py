ca0b6a3d84436a461e9ed71dd1b7ce7e0cc82b6e
aviescobar <91568563+aviescobar@users.noreply.github.com>
2024-12-20 21:46:37 -0600

    Update mostrar.py

--- Mostrar-serv/mostrar.py
+++ Mostrar-serv/mostrar.py
@@ -10,4 +10,6 @@ def start_server(host='0.0.0.0', port=9999):
   server_socket.bind((host, port))
   server_socket.listen(1)
 
-print(f"[INFO] Servidor escuchando en {host}:{port}")
+  print(f"[INFO] Servidor escuchando en {host}:{port}")
+
+  try:
